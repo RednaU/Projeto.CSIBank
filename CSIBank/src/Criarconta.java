60bd28825138915f6742ad1f4fd0b26b561856e2
Uander <uandermatheus2015@gmail.com>
2019-09-19 21:56:42 -0300

    atualizando

--- CSIBank/src/Criarconta.java
+++ CSIBank/src/Criarconta.java
@@ -2,8 +2,20 @@
 public class Criarconta {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Conta conta1 = new Conta();
+		conta1.saldo = 80.00;
+		conta1.agencia = 1996;
+		conta1.numero = 123729;
+		conta1.titular = "Matheus";
+		
+		Conta conta2 = new Conta();
+		conta2.saldo = 80.00;
+		conta2.agencia = 1996;
+		conta2.numero = 123729;
+		conta2.titular = "Matheus";
+			   
+}
 
-	}
 
 }
