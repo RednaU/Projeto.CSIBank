b02be4a18b393a84ff12ffd6568b5376fbefcc00
Uander <uandermatheus2015@gmail.com>
2019-10-21 21:53:02 -0300

    Novos Conteúdos

--- CSIBank/src/Criarconta.java
+++ CSIBank/src/Criarconta.java
@@ -7,14 +7,23 @@ public class Criarconta {
 		conta1.saldo = 80.00;
 		conta1.agencia = 1996;
 		conta1.numero = 123729;
-		conta1.titular = "Matheus";
+		conta1.titular.nome = "Matheus";
+		conta1.titular.email = "";
+		conta1.titular.CPF = "";
+		
 		
 		Conta conta2 = new Conta();
 		conta2.saldo = 80.00;
 		conta2.agencia = 1996;
 		conta2.numero = 123729;
-		conta2.titular = "Matheus";
+		conta2.titular.nome = "Matheus";
+		conta2.titular.email = "";
+		conta2.titular.CPF = "";
+		
+		
 			   
+		
+		
 		if(conta1 == conta2) {
 			System.out.println("Contas iguais");
 		} else {
