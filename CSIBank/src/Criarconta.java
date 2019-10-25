8808267db223b519453589d38aedf74fe2e510ab
Uander <uandermatheus2015@gmail.com>
2019-10-24 21:52:53 -0300

    Testa Associação

--- CSIBank/src/Criarconta.java
+++ CSIBank/src/Criarconta.java
@@ -2,35 +2,47 @@
 public class Criarconta {
 
 	public static void main(String[] args) {
-		
+	
 		Conta conta1 = new Conta();
-		conta1.saldo = 80.00;
-		conta1.agencia = 1996;
-		conta1.numero = 123729;
-		conta1.titular.nome = "Matheus";
-		conta1.titular.email = "";
-		conta1.titular.CPF = "";
-		
+		conta1.saldo = 100.00;
+		conta1.agencia = 1234;
+		conta1.numero = 123456;
 		
 		Conta conta2 = new Conta();
-		conta2.saldo = 80.00;
-		conta2.agencia = 1996;
-		conta2.numero = 123729;
-		conta2.titular.nome = "Matheus";
-		conta2.titular.email = "";
-		conta2.titular.CPF = "";
+		conta2.saldo = 100.00;
+		conta2.agencia = 1234;
+		conta2.numero = 123456;
+
+		Titular titular = new Titular();
+
 		
-		
-			   
-		
+		conta2.titular = titular;
 		
 		if(conta1 == conta2) {
-			System.out.println("Contas iguais");
+			System.out.println("Contas iguais!");
 		} else {
 			System.out.println("Contas diferentes");
+			System.out.println(conta1);
+			System.out.println(conta2);
 		}
 		
-}
+		if(conta1.agencia == conta2.agencia) {
+			System.out.println("Agências iguais!");
+		} else {
+			System.out.println("Agências diferentes");
+		}
+		
+		int numero1 = 5;
+		int numero2 = 5;
+		
+		if(numero1 == numero2) {
+			System.out.println("Números iguais");
+		} else {
+			System.out.println("Números diferentes");
+		}
+		
 
+	}
 
-}
+}	
+
